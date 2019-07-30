26c9bbf94b859a3ad7532148ab0ebc20b4cdbedf
Vinicius Ferreira <vferreira@mastertech.tech>
2019-07-30 14:44:14 -0300

    Criar resolucao com bonus

--- tabuada.py
+++ tabuada.py
@@ -1,6 +1,14 @@
 
-numero = int(input('Digite um numero'))
+opicao = int(input('Digite:\n1 - para tabuada completa\n2 - Para tabuada específico\n:'))
 
-for x in range(1, 11):
-    resultado = numero*x
-    print(numero, 'X', x, '=', resultado)
+if opicao == 1:
+    for x in range(1, 11):
+        for y in range(1, 11):
+            resultado = x*y
+            print(x, 'X', y, '=', resultado)
+elif opicao == 2:
+    numero = int(input('Digite um numero\n:'))
+
+    for x in range(1, 11):
+        resultado = numero*x
+        print(numero, 'X', x, '=', resultado)
